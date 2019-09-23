b62b2339d9b7fcb57c6600223478660706d3a91f
David Jones <davidjones@sportronics.com.au>
2019-09-23 16:49:36 +1000

    Version No 1.1.2 set

--- AzureIoTHubToolbox/Properties/AssemblyInfo.cs
+++ AzureIoTHubToolbox/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AzureIoTHubWorkBench")]
+[assembly: AssemblyTitle("Azure IoT Hub Work Bench")]
 [assembly: AssemblyDescription("A UWP Xaml appfor getting a QUICK Quickstart with Azure IoT Hub features")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Sportronics")]
 [assembly: AssemblyProduct("AzureIoTHubWorkBench")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("© 2019 Sportronics")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.2.0")]
+[assembly: AssemblyFileVersion("1.1.2.0")]
 [assembly: ComVisible(false)]
